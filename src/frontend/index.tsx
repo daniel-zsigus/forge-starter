937d8fffc7020f05d1c21f7bc646697108a079e5
Daniel Zsigus <daniel.zsigus@everit.biz>
2025-09-30 20:33:19 +0000

    Add feature-3 text display in the frontend

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -15,6 +15,7 @@ const App = () => {
     <>
       <Text>Hello world from frontend!</Text>
       <Text>{data || "Loading text from backend..."}</Text>
+      <Text>Feature-3</Text>
     </>
   );
 };
